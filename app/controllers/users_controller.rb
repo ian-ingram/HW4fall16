61a1f7de67d3fc2d4fd8f4b47e1adff46a3ca06d
Ian Ingram <ian-ingram@uiowa.edu>
2016-09-26 14:36:33 +0000

    add users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,43 @@
+class UsersController < ApplicationController
+  def user_params
+    params.require(:user).permit(:user_id, :email, :session_token, :timestamps)
+  end
+
+  def show
+    id = params[:id] # retrieve movie ID from URI route
+    @user = User.find(id) # look up movie by unique ID
+    # will render app/views/movies/show.<extension> by default
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    # default: render 'new' template
+  end
+
+  def create
+    @user = User.create!(user_params)
+    flash[:notice] = "#{@user.email} was successfully created."
+    redirect_to users_path
+  end
+
+  def edit
+    @user = User.find params[:id]
+  end
+
+  def update
+    @user = User.find params[:id]
+    @user.update_attributes!(user_params)
+    flash[:notice] = "#{@user.email} was successfully updated."
+    redirect_to user_path(@user)
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:notice] = "User '#{@user.email}' deleted."
+    redirect_to users_path
+  end
+end
